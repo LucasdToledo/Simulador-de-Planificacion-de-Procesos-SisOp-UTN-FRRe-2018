f963d28b1203c7679219e7c8bf23530d7810d441
LucasdToledo <40505862+LucasdToledo@users.noreply.github.com>
2018-10-04 10:51:04 -0300

    Primer Comit de prueba
    Intento numero 294837249 de subir el programa

---
+++ Simulador de Procesos/src/simulador/de/procesos/SimuladorDeProcesos.java
@@ -0,0 +1,20 @@
+
+package simulador.de.procesos;
+
+/**
+ *
+ * @author fvg
+ */
+public class SimuladorDeProcesos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        String texto;
+        texto="Lucas";
+        System.out.println("Yo soy "+texto);
+    }
+    
+}
