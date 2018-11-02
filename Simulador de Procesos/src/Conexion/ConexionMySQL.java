e78f415e6f7c0a45589baff4eebf43bd4d1c3be9
LucasdToledo <40505862+LucasdToledo@users.noreply.github.com>
2018-11-02 15:09:48 -0300

    Actualizacion de Lucas
    Cambios en las clases por persistencia de errores

---
+++ Simulador de Procesos/src/Conexion/ConexionMySQL.java
@@ -0,0 +1,46 @@
+
+package Conexion;
+
+import java.sql.*;
+
+/**
+ *
+ * @author lucas
+ */
+public class ConexionMySQL {
+
+    // Librería de MySQL
+    public String driver = "com.mysql.jdbc.Driver";
+
+    // Nombre de la base de datos
+    public String database = "databasemovies";
+
+    // Host
+    public String hostname = "localhost";
+
+    // Puerto
+    public String port = "3306";
+
+    // Ruta de nuestra base de datos (desactivamos el uso de SSL con "?useSSL=false")
+    public String url = "jdbc:mysql://" + hostname + ":" + port + "/" + database + "?useSSL=false";
+
+    // Nombre de usuario
+    public String username = "root";
+
+    // Clave de usuario
+    public String password = "";
+
+    public Connection conectarBD() {
+        Connection conn = null;
+
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+
+}
