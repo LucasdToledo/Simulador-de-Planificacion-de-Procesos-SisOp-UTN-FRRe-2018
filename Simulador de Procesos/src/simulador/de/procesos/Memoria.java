4cf5df65e04f149b42415c4d6cfbbe78d40fd786
LucasdToledo <40505862+LucasdToledo@users.noreply.github.com>
2018-11-07 12:58:16 -0300

    Actualizacion 7-11
    Importantes cambios

--- Simulador de Procesos/src/simulador/de/procesos/Memoria.java
+++ Simulador de Procesos/src/simulador/de/procesos/Memoria.java
@@ -1,30 +1,48 @@
 
 package simulador.de.procesos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Memoria {
+    //Tamaño de la memoria
     private int tamaño;
+    //Variable que determina si la memoria es estática o dinámica
+    private boolean tipo;
+    //Lista de particiones
     private List<Particion> ListaParticiones;
 
     public int getTamaño() {
         return tamaño;
     }
-
     public void setTamaño(int tamaño) {
         this.tamaño = tamaño;
     }
 
+    public boolean isTipo() {
+        return tipo;
+    }
+    //True = Dinámica; False=Estática
+    public void setTipo(boolean tipo) {
+        this.tipo = tipo;
+    }
+    
     public List<Particion> getListaParticiones() {
         return ListaParticiones;
     }
-
     public void setListaParticiones(List<Particion> ListaParticiones) {
         this.ListaParticiones = ListaParticiones;
     }
     
-    public void CrearMemoria (int _tam, List<Particion> _ListaP) {
+    public void CrearMemoria (int _tam, boolean _tipo) {
         tamaño = _tam;
-        ListaParticiones = _ListaP;
+        tipo = _tipo;
+        List <Particion> ListadeParticiones = new ArrayList();
+        ListaParticiones = ListadeParticiones;
+        if(tipo){
+             Particion part = new Particion();
+             part.CrearParticion(0, tamaño, true);
+             ListaParticiones.add(part);
+        }
     }
 }
