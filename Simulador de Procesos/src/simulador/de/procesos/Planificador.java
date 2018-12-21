49c13579f0988ce3770731a09cdc7a41b1d020af
LucasdToledo <40505862+LucasdToledo@users.noreply.github.com>
2018-12-21 18:44:04 -0300

    a

--- Simulador de Procesos/src/simulador/de/procesos/Planificador.java
+++ Simulador de Procesos/src/simulador/de/procesos/Planificador.java
@@ -77,49 +77,7 @@ public class Planificador {
         if (!colaListos.isEmpty()){
             switch(algorit){
                 case(1):    //Round Robin
-                    /*
-                    //Pregunto si existe algo dentro de la primer posicion de la lista, si hay cargo mis variables 
-                    if ( Estado == 1 && !(colaListos.isEmpty())){
-                        Rafaga= colaListos.get(iRR).getCicloCPU() ;
-                        Quantum=3;
-                        ResiduoRafaga= colaListos.get(iRR).getCicloCPU() ;
-                        Ejecucion = 1;
-                        Estado = 0;
-                    }else{
-                       Ejecucion = 0;
-                    }
-
-                    if (Quantum != 0 && Ejecucion == 1) {
-                        if (ResiduoRafaga != 0){
-                        ResiduoRafaga = ResiduoRafaga - 1;
-                        Quantum = Quantum - 1;
-                        }else{
-                            Estado=1;
-                        }
-                    }else{
-                        if (Quantum == 0 && Ejecucion == 1 ){
-                            Estado = 1;
-                            colaListos.get(iRR).setCicloCPU(ResiduoRafaga);
-                            iRR++;
-                        }
-                    }
-                    */
-                    //Si todavía no se termina el quantum decrece el ResiduoRafaga
-                    if (ResiduoRafaga > 1){
-                        ResiduoRafaga--;
-                    }
-                    else {
-                        //Si el contador es mayor al tamaño de la lista lo pongo a cero para que reinicie
-                        if (colaListos.size()<= iRR){
-                            iRR = 0;
-                        }
-                        //Si no es tan grande lo incremento
-                        else {
-                            iRR++;
-                        }
-                        //Como es un nuevo proceso reinicio el quantum
-                        ResiduoRafaga = quantum;
-                    }
+                    
                     proaux = nuevaColaListos.get(iRR);
                     
                     //Se llama SJF pero cumple la misma función en RR
@@ -162,6 +120,23 @@ public class Planificador {
                                 }
                             }
                         }
+                    }
+                    
+                    //Si todavía no se termina el quantum decrece el ResiduoRafaga
+                    if (ResiduoRafaga > 0){
+                        ResiduoRafaga--;
+                    }
+                    else {
+                        //Si el contador es mayor al tamaño de la lista lo pongo a cero para que reinicie
+                        if (colaListos.size()<= iRR){
+                            iRR = 0;
+                        }
+                        //Si no es tan grande lo incremento
+                        else {
+                            iRR++;
+                        }
+                        //Como es un nuevo proceso reinicio el quantum
+                        ResiduoRafaga = quantum-1;
                     }
                     
                     break;
